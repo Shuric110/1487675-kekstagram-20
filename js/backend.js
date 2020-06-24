92736a8f1b876feccd9f9ade74ed181d7db1265f
Shuric110 <65159520+Shuric110@users.noreply.github.com>
2020-06-24 07:51:06 +0300

    Переименование модуля network в http

--- js/backend.js
+++ js/backend.js
@@ -5,7 +5,7 @@
   var PICTURES_DATA_URL = 'https://javascript.pages.academy/kekstagram/data';
 
   var loadPicturesData = function (onLoad, onError) {
-    window.network.doGetQuery(PICTURES_DATA_URL, onLoad, onError);
+    window.http.doGetQuery(PICTURES_DATA_URL, onLoad, onError);
   };
 
   window.backend = {
